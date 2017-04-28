2577a5a4398b334da2a1b24bffb9c016b8d7b5b1
ZimTis <zimtis@t-online.de>
2017-04-28 15:26:30 +0200

    bug with callable

--- src/filter/attributeFilter/CallableFilter.php
+++ src/filter/attributeFilter/CallableFilter.php
@@ -9,7 +9,7 @@ use zimtis\arrayvalidation\CallableBox;
 /**
  *
  * @author ZimTis
- *        
+ *
  * @since 0.0.9 added
  */
 class CallableFilter extends Filter
@@ -43,19 +43,21 @@ class CallableFilter extends Filter
         $arguments = $this->keyValidator->getCallableArguments();
         $arguments = is_null($arguments) || ! is_array($arguments) ? array() : $arguments;
         if ($callable instanceof CallableBox) {
-            
+
             // if callable is a string, resolve the string, eather as a static call to a functio or a non static call to a functio of a class
             // if call to a function, static function, or using the closure, user below code
             // $result = call_user_func($callable, $value, $arguments);
-            
+
             // otherwhise, use below code
             $result = call_user_func($callable->getCallable(), $value, $arguments);
             if (! is_bool($result)) {
                 throw new \Exception(sprintf('%s , Callable must return a boolean', $this->keyValidator->getFullName()));
             }
-            return $result;
+            if (! $result) {
+                throw new \Exception(sprintf('%s , Callable returnes false', $this->keyValidator->getFullName()));
+            }
         }
-        
-        return true;
+
+        parent::validate($value);
     }
 }
